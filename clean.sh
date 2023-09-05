c5f9ffa5be0f27d239fe7b62c8b1a0cfa5954c9a
Jayanring <956165026@qq.com>
2023-09-05 15:28:06 +0800

    optim clean

--- clean.sh
+++ clean.sh
@@ -6,6 +6,8 @@ source ./env.sh
 kubectl delete -f ./yamls/l2-explorer/sts_explorer.yaml -n $NAME_SPACE
 kubectl delete -f ./yamls/l2-sequencer/sts_sequencer.yaml -n $NAME_SPACE
 kubectl delete -f ./yamls/l2-verifier/sts_verifier.yaml -n $NAME_SPACE
+kubectl delete -f ./yamls/l2-init/ -n$NAME_SPACE
+kubectl delete -f ./yamls/test.yaml -n$NAME_SPACE
 
 kubectl delete pvc l2-config-pvc -n $NAME_SPACE
 kubectl delete pvc datadir-sequencer-l2-sequencer-0 -n $NAME_SPACE
